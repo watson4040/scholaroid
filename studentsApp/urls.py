1d5674aab57e41ad4bcf57f12792ebcfa827e462
S & T Baber Lounge <147267845+watson4040@users.noreply.github.com>
2026-07-01 20:24:59 +0200

    Update studentsApp URLs

--- studentsApp/urls.py
+++ studentsApp/urls.py
@@ -1,10 +1,1 @@
-from django.urls import path, include
-from . import views
-from .views import change_password
-from studentsApp.views import enrollment_request, enrollment_success   # <-- correct import
 
-urlpatterns = [
-    # ... your existing paths ...
-    path('enroll/', enrollment_request, name='enroll'),
-    path('enroll/success/', enrollment_success, name='enrollment_success'),
-]
