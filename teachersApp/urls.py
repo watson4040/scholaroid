39f00a2ff08e0512cf42ca9ee9f6415e6a04103f
S & T Baber Lounge <147267845+watson4040@users.noreply.github.com>
2026-07-10 15:06:13 +0200

    Add pupil_report URL

--- teachersApp/urls.py
+++ teachersApp/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('', views.dashboard_teacher, name='dashboard_teacher'),
     path('class/<int:class_id>/', views.teacher_class_detail, name='teacher_class_detail'),
     path('report/<int:pupil_id>/', views.pupil_report_create_or_edit, name='pupil_report'),
-    # Optional: with term and year
-    path('report/<int:pupil_id>/<str:term>/<str:year>/', views.pupil_report_create_or_edit, name='pupil_report_with_term'),
 ]
