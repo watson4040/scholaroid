e06d2787521d440d144a5f4ea54af6fa5287e9b3
S & T Baber Lounge <147267845+watson4040@users.noreply.github.com>
2026-07-10 14:50:49 +0200

    Add PupilReportForm to enable teacher report creation

--- teachersApp/forms.py
+++ teachersApp/forms.py
@@ -1,24 +1,27 @@
 from django import forms
-from .models import Teacher
+from .models import Teacher, PupilReport
 from classesApp.models import ClassRoom, Subjects
 
+# ---- Admin Teacher Form ----
 class TeacherAdminForm(forms.ModelForm):
-    subject = forms.ModelMultipleChoiceField(
-        queryset=Subjects.objects.all(),
-        widget=forms.SelectMultiple(attrs={'class': 'form-select', 'size': 10})
-    )
-    assigned_class = forms.ModelMultipleChoiceField(
-        queryset=ClassRoom.objects.all(),
-        widget=forms.SelectMultiple(attrs={'class': 'form-select', 'size': 10})
-    )
-
     class Meta:
         model = Teacher
-        fields = ['subject', 'assigned_class']
+        fields = ['user', 'subject', 'assigned_class', 'hire_date']
+        widgets = {
+            'user': forms.Select(attrs={'class': 'form-select'}),
+            'subject': forms.SelectMultiple(attrs={'class': 'form-select'}),
+            'assigned_class': forms.SelectMultiple(attrs={'class': 'form-select'}),
+            'hire_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+        }
 
-    def save(self, commit=True):
-        instance = super().save(commit=False)
-        if commit:
-            instance.save()
-            self.save_m2m()
-        return instance
+# ---- Pupil Report Form (for teacher comments) ----
+class PupilReportForm(forms.ModelForm):
+    class Meta:
+        model = PupilReport
+        fields = ['term', 'academic_year', 'comment', 'is_submitted']
+        widgets = {
+            'term': forms.Select(attrs={'class': 'form-select'}),
+            'academic_year': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g. 2025/2026'}),
+            'comment': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Write your comments about the pupil...'}),
+            'is_submitted': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
