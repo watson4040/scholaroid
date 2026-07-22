9decdda83b6cdfb065a75c369913da323b31b288
S & T Baber Lounge <147267845+watson4040@users.noreply.github.com>
2026-07-22 13:49:17 +0200

    Fix user admin: handle role field properly

--- accountsApp/admin.py
+++ accountsApp/admin.py
@@ -8,6 +8,12 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'role')
 
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()
+        return user
+
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = User
@@ -22,10 +28,9 @@ class UserAdmin(BaseUserAdmin):
     search_fields = ('username', 'email')
     ordering = ('username',)
     
-    # ---------- REMOVED profile_photo completely ----------
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('email', 'first_name', 'last_name')}),  # profile_photo removed
+        ('Personal info', {'fields': ('email', 'first_name', 'last_name')}),
         ('Role', {'fields': ('role',)}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
