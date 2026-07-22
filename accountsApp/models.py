10d887786c2a88f2f089b6c43f01211bf389a654
S & T Baber Lounge <147267845+watson4040@users.noreply.github.com>
2026-07-22 14:16:41 +0200

    Add default value for role field in User model

--- accountsApp/models.py
+++ accountsApp/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
         ('parent', 'Parent'),
     )
     
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')  # Added default
     profile_photo = models.ImageField(upload_to='profiles/', blank=True, null=True)
 
 class Notice(models.Model):
